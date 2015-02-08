a8d58c9360834ce70472f5719227f76fcf8c52d7
Joel Martin <github@martintribe.org>
2015-02-07 21:03:43 -0600

    matlab: step2

--- matlab/step0_repl.m
+++ matlab/step0_repl.m
@@ -1,25 +1,28 @@
 function step0_repl(varargin), main(varargin), end
 
+% read
 function ret = READ(str)
     ret = str;
 end
 
+% eval
 function ret = EVAL(ast, env)
     ret = ast;
 end
 
+% print
 function ret = PRINT(ast)
     ret = ast;
 end
 
-function ret = rep(str)
-    ret = PRINT(EVAL(READ(str), ''));
+% REPL
+function ret = rep(str, env)
+    ret = PRINT(EVAL(READ(str), env));
 end
 
 function main(args)
     while (true)
         line = input('user> ', 's');
-        fprintf('%s\n', rep(line));
+        fprintf('%s\n', rep(line, ''));
     end
 end
-
--- matlab/step2_eval.m
+++ matlab/step2_eval.m
@@ -6,8 +6,31 @@ function ret = READ(str)
 end
 
 % eval
+function ret = eval_ast(ast, env)
+    switch class(ast)
+    case 'types.Symbol'
+        ret = env(ast.name);
+    case 'cell'
+        ret = {};
+        for i=1:length(ast)
+            ret{end+1} = EVAL(ast{i}, env);
+        end
+    otherwise
+        ret = ast;
+    end
+end
+
 function ret = EVAL(ast, env)
-    ret = ast;
+    if ~iscell(ast),
+        ret = eval_ast(ast, env);
+        return;
+    end
+
+    % apply
+    el = eval_ast(ast, env);
+    f = el{1};
+    args = el(2:end);
+    ret = f(args{:});
 end
 
 % print
@@ -21,12 +44,16 @@ function ret = rep(str, env)
 end
 
 function main(args)
+    repl_env = containers.Map( ...
+        {'+', '-', '*', '/'}, ...
+        {@(a,b) a+b, @(a,b) a-b, @(a,b) a*b, @(a,b) floor(a/b)});
+
     %cleanObj = onCleanup(@() disp('*** here1 ***'));
     while (true)
         line = input('user> ', 's');
         if strcmp(strtrim(line),''), continue, end
         try
-            fprintf('%s\n', rep(line, ''));
+            fprintf('%s\n', rep(line, repl_env));
         catch err
             fprintf('Error: %s\n', err.message);
             fprintf('%s\n', getReport(err, 'extended'));
