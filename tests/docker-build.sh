4a55f10c42f0b7ee594bed1e448c31d23cd66417
Joel Martin <github@martintribe.org>
2015-03-12 17:41:55 -0500

    docker: build script executable. Skip matlab automatically.

--- tests/docker-build.sh
+++ tests/docker-build.sh
@@ -3,4 +3,4 @@
 IMAGE_NAME=${IMAGE_NAME:-mal-test-ubuntu-utopic}
 GIT_TOP=$(git rev-parse --show-toplevel)
 
-docker build -t "${IMAGE_NAME}" "${GIT_TOP}/tests/"
+docker build -t "${IMAGE_NAME}" "${GIT_TOP}/tests/docker"
