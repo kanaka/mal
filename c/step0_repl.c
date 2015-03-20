bf2cb97aaae9129763d2842b4e27cbf62dcea4d5
Joel Martin <github@martintribe.org>
2015-03-20 08:35:53 -0500

    C: remove extraneous glib includes.

--- c/step0_repl.c
+++ c/step0_repl.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <glib.h>
 
 #ifdef USE_READLINE
   #include <readline/readline.h>
--- c/step1_read_print.c
+++ c/step1_read_print.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <glib.h>
 
 #include "types.h"
 #include "readline.h"
