c6b985d53a1b5217da410cfd4df45f5dd6c0997d
Nala Ginrut <nalaginrut@gmail.com>
2015-03-24 18:37:05 +0800

    updated guile/step0_repl.scm

--- guile/step0_repl.scm
+++ guile/step0_repl.scm
@@ -13,7 +13,7 @@
 ;;  You should have received a copy of the GNU General Public License
 ;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(include "readline.scm")
+(import (readline))
 
 (define (READ) (readline "user> "))
 
