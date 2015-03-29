7728b50bd5ff589472ecbfa951c6dca73edb2128
Keith Rollin <keith.rollin@gmail.com>
2015-03-29 00:55:56 -0700

    optimization: for small Environment records, store names/values in hard-coded slots rather than general dictionary

--- swift/env.swift
+++ swift/env.swift
@@ -7,6 +7,8 @@ import Foundation
 typealias EnvironmentVars = [MalSymbol: MalVal]
 
 let kSymbolAmpersand = MalSymbol(symbol: "&")
+let kNil = MalNil()
+let kNilSymbol = MalSymbol(symbol: "")
 
 class Environment {
     init(outer: Environment?) {
@@ -21,7 +23,7 @@ class Environment {
                 if index < exprs.count {
                     set(sym, exprs[index])
                 } else {
-                    set(sym, MalNil())
+                    set(sym, kNil)
                 }
                 continue
             }
@@ -44,19 +46,60 @@ class Environment {
             set(rest_sym, MalList(slice: rest))
             break
         }
-        return MalNil()
+        return kNil
     }
 
     func set(sym: MalSymbol, _ value: MalVal) -> MalVal {
-        data[sym] = value
+        if num_bindings == 0 {
+            slot_name0 = sym; slot_value0 = value; ++num_bindings
+        } else if num_bindings == 1 {
+            if slot_name0 == sym { slot_value0 = value }
+            else { slot_name1 = sym; slot_value1 = value; ++num_bindings }
+        } else if num_bindings == 2 {
+            if slot_name0 == sym { slot_value0 = value }
+            else if slot_name1 == sym { slot_value1 = value }
+            else { slot_name2 = sym; slot_value2 = value; ++num_bindings }
+        } else if num_bindings == 3 {
+            if slot_name0 == sym { slot_value0 = value }
+            else if slot_name1 == sym { slot_value1 = value }
+            else if slot_name2 == sym { slot_value2 = value }
+            else { slot_name3 = sym; slot_value3 = value; ++num_bindings }
+        } else if num_bindings == 4 {
+            if slot_name0 == sym { slot_value0 = value }
+            else if slot_name1 == sym { slot_value1 = value }
+            else if slot_name2 == sym { slot_value2 = value }
+            else if slot_name3 == sym { slot_value3 = value }
+            else {
+                data[slot_name0] = slot_value0
+                data[slot_name1] = slot_value1
+                data[slot_name2] = slot_value2
+                data[slot_name3] = slot_value3
+                data[sym] = value; ++num_bindings
+            }
+        } else {
+            data[sym] = value
+        }
         return value
     }
 
     func get(sym: MalSymbol) -> MalVal? {
-        if let val = data[sym] { return val }
+        if num_bindings > 4 { if let val = data[sym] { return val } }
+        if num_bindings > 3 { if slot_name3 == sym { return slot_value3 } }
+        if num_bindings > 2 { if slot_name2 == sym { return slot_value2 } }
+        if num_bindings > 1 { if slot_name1 == sym { return slot_value1 } }
+        if num_bindings > 0 { if slot_name0 == sym { return slot_value0 } }
         return outer?.get(sym)
     }
 
     private var outer: Environment?
     private var data = EnvironmentVars()
+    private var num_bindings = 0
+    private var slot_name0: MalSymbol = kNilSymbol
+    private var slot_name1: MalSymbol = kNilSymbol
+    private var slot_name2: MalSymbol = kNilSymbol
+    private var slot_name3: MalSymbol = kNilSymbol
+    private var slot_value0: MalVal = kNil
+    private var slot_value1: MalVal = kNil
+    private var slot_value2: MalVal = kNil
+    private var slot_value3: MalVal = kNil
 }
