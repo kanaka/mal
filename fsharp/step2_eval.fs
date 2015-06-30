52bc43adf168666bf9ecb725bc89116d2ed2edbd
Peter Stephens <code@diligentsoftware.com>
2015-06-29 22:57:47 -0500

    fsharp: eval 'sharded across the time dimension' for step2 & step3.

--- fsharp/step2_eval.fs
+++ fsharp/step2_eval.fs
@@ -1,7 +1,23 @@
 module REPL
     open System
+    open Types
 
-    let read input =
+    let rec eval_ast env = function
+        | Symbol(sym) -> Env.get env sym
+        | List(lst) -> lst |> List.map (eval env) |> List
+        | Vector(seg) -> seg |> Seq.map (eval env) |> Array.ofSeq |> Node.ofArray
+        | Map(map) -> map |> Map.map (fun k v -> eval env v) |> Map
+        | node -> node
+
+    and eval env = function
+        | List(_) as node ->
+            let resolved = node |> eval_ast env
+            match resolved with
+            | List(Func(_, f, _, _, [])::rest) -> f rest
+            | _ -> raise <| Error.errExpectedX "function"
+        | node -> node |> eval_ast env
+
+    let READ input =
         try
             Reader.read_str input
         with
@@ -9,26 +25,26 @@ module REPL
             printfn "%s" msg
             []
 
-    let eval env ast =
+    let EVAL env ast =
         try
-            Some(Eval.eval env ast)
+            Some(eval env ast)
         with
         | Error.EvalError(msg) 
         | Error.ReaderError(msg) ->
             printfn "%s" msg
             None
 
-    let print v =
+    let PRINT v =
         v
         |> Seq.singleton
         |> Printer.pr_str
         |> printfn "%s"
 
-    let rep env input =
-        read input
+    let REP env input =
+        READ input
         |> Seq.ofList
-        |> Seq.choose (fun form -> eval env form)
-        |> Seq.iter (fun value -> print value)
+        |> Seq.choose (fun form -> EVAL env form)
+        |> Seq.iter (fun value -> PRINT value)
 
     let getReadlineMode (args : string array) =
         if args.Length > 0 && args.[0] = "--raw" then
@@ -43,5 +59,5 @@ module REPL
         match Readline.read "user> " mode with
         | null -> 0
         | input -> 
-            rep env input
+            REP env input
             main args
--- fsharp/step3_env.fs
+++ fsharp/step3_env.fs
@@ -1,7 +1,64 @@
 module REPL
     open System
+    open Types
 
-    let read input =
+    let mapPairs f (source : seq<_>) =
+        use iter = source.GetEnumerator()
+        let rec loop () =
+            if iter.MoveNext() then
+                let first = iter.Current
+                if not (iter.MoveNext()) then raise <| Error.errExpectedX "even node count"
+                let second = iter.Current
+                f first second
+                loop ()
+        loop ()
+
+    let rec eval_ast env = function
+        | Symbol(sym) -> Env.get env sym
+        | List(lst) -> lst |> List.map (eval env) |> List
+        | Vector(seg) -> seg |> Seq.map (eval env) |> Array.ofSeq |> Node.ofArray
+        | Map(map) -> map |> Map.map (fun k v -> eval env v) |> Map
+        | node -> node
+
+    and defBang env = function
+        | sym::node::[] ->
+            match sym with
+            | Symbol(sym) ->
+                let node = eval env node
+                Env.set env sym node
+                node
+            | _ -> raise <| Error.errExpectedX "symbol"
+        | _ -> raise <| Error.wrongArity ()
+
+    and setBinding env first second =
+        let s = match first with
+                | Symbol(s) -> s
+                | _ -> raise <| Error.errExpectedX "symbol"
+        let form = eval env second
+        Env.set env s form
+
+    and letStar env = function
+        | bindings::form::[] ->
+            let newEnv = Env.makeNew env [] []
+            let binder = setBinding newEnv
+            match bindings with
+            | List(lst) -> lst |> mapPairs binder
+            | Vector(vec) -> vec |> mapPairs binder
+            | _ -> raise <| Error.errExpectedX "list or vector"
+            eval newEnv form
+        | _ -> raise <| Error.wrongArity ()
+
+    and eval env = function
+        | List(Symbol("def!")::rest) -> defBang env rest
+        | List(Symbol("let*")::rest) -> letStar env rest
+        | List(_) as node ->
+            let resolved = node |> eval_ast env
+            match resolved with
+            | List(Func(_, f, _, _, [])::rest) -> f rest
+            | _ -> raise <| Error.errExpectedX "function"
+        | node -> node |> eval_ast env
+
+    let READ input =
         try
             Reader.read_str input
         with
@@ -9,26 +66,26 @@ module REPL
             printfn "%s" msg
             []
 
-    let eval env ast =
+    let EVAL env ast =
         try
-            Some(Eval.eval env ast)
+            Some(eval env ast)
         with
         | Error.EvalError(msg) 
         | Error.ReaderError(msg) ->
             printfn "%s" msg
             None
 
-    let print v =
+    let PRINT v =
         v
         |> Seq.singleton
         |> Printer.pr_str
         |> printfn "%s"
 
-    let rep env input =
-        read input
+    let REP env input =
+        READ input
         |> Seq.ofList
-        |> Seq.choose (fun form -> eval env form)
-        |> Seq.iter (fun value -> print value)
+        |> Seq.choose (fun form -> EVAL env form)
+        |> Seq.iter (fun value -> PRINT value)
 
     let getReadlineMode (args : string array) =
         if args.Length > 0 && args.[0] = "--raw" then
@@ -44,6 +101,6 @@ module REPL
             match Readline.read "user> " mode with
             | null -> 0
             | input ->
-                rep env input
+                REP env input
                 loop ()
         loop ()
