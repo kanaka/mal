ee6614ec66f6507f89a081b2d22fcacab0ad4973
ekmartin <mail@ekmartin.no>
2015-09-05 01:51:58 +0200

    Step 2 using agent as env

---
+++ elixir/lib/mal/env.ex
@@ -0,0 +1,48 @@
+defmodule Mal.Env do
+  def initialize(outer \\ nil) do
+    Agent.start_link(fn ->
+      %{outer: outer, env: %{}}
+    end)
+  end
+
+  def set(pid, key, value) do
+    Agent.update(pid, fn map ->
+      Map.put(map.env, key, value)
+    end)
+  end
+
+  def merge(pid, env_values) do
+    Agent.update(pid, fn map ->
+      %{map | :env => Map.merge(map.env, env_values)}
+    end)
+  end
+
+  defp find_callback(map, key) do
+    IO.inspect(map.env)
+  end
+
+  def find(pid, key) do
+    Agent.get(pid, fn map ->
+      case Map.has_key?(map.env, key) do
+        true -> pid
+        false -> map.outer && find(map.outer, key)
+      end
+    end)
+  end
+
+  def retrieve_key(pid, key) do
+    Agent.get(pid, fn map ->
+      case Map.fetch(map.env, key) do
+        {:ok, value} -> {:ok, value}
+        :error -> :not_found
+      end
+    end)
+  end
+
+  def get(pid, key) do
+    case find(pid, key) do
+      nil -> :not_found
+      env -> retrieve_key(env, key)
+    end
+  end
+end
---
+++ elixir/lib/mix/tasks/step3_env.ex
@@ -0,0 +1,59 @@
+defmodule Mix.Tasks.Step3Env do
+  @initial_env %{
+    "+" => &+/2,
+    "-" => &-/2,
+    "*" => &*/2,
+    "/" => &div/2
+  }
+
+  def run(_) do
+    {:ok, env} = Mal.Env.initialize()
+    Mal.Env.merge(env, @initial_env)
+    main(env)
+  end
+
+  def main(env) do
+    IO.write(:stdio, "user> ")
+    IO.read(:stdio, :line)
+      |> read_eval_print(env)
+
+    main(env)
+  end
+
+  def eval_ast(ast, env) when is_list(ast) do
+    Enum.map(ast, fn elem -> eval(elem, env) end)
+  end
+
+  def eval_ast({:symbol, symbol}, env) do
+    case Mal.Env.get(env, symbol) do
+      {:ok, value} -> value
+      :not_found -> throw({:error, "invalid symbol #{symbol}"})
+    end
+  end
+
+  def eval_ast(ast, _env), do: ast
+
+  def read(input) do
+    Mal.Reader.read_str(input)
+  end
+
+  def eval(ast, env) when is_list(ast) do
+    [func | args] = eval_ast(ast, env)
+    apply(func, args)
+  end
+
+  def eval(ast, env), do: eval_ast(ast, env)
+
+  def print(value) do
+    IO.puts(Mal.Printer.print_str(value))
+  end
+
+  def read_eval_print(:eof, _env), do: exit(0)
+  def read_eval_print(line, env) do
+    read(line)
+      |> eval(env)
+      |> print
+  catch
+    {:error, message} -> IO.puts("Error: #{message}")
+  end
+end
