d34837dd38ba48ad65d4924bdd6b262315a03f2a
ekmartin <mail@ekmartin.no>
2015-09-06 00:33:13 +0200

    Implement core functions

--- elixir/lib/mal/core.ex
+++ elixir/lib/mal/core.ex
@@ -1,15 +1,28 @@
 defmodule Mal.Core do
   def namespace do
     %{
-      "+" => &add/1,
-      "-" => &subtract/1,
-      "*" => &multiply/1,
-      "/" => &divide/1,
+      "+" => fn [a, b] -> a + b end,
+      "-" => fn [a, b] -> a - b end,
+      "*" => fn [a, b] -> a * b end,
+      "/" => fn [a, b] -> div(a, b) end,
+      "=" => fn [a, b] -> a == b end,
+      ">" => fn [a, b] -> a > b end,
+      "<" => fn [a, b] -> a < b end,
+      "<=" => fn [a, b] -> a <= b end,
+      ">=" => fn [a, b] -> a >= b end,
+      "list" => fn args -> args end,
+      "list?" => &list?/1,
+      "empty?" => &empty?/1,
+      "count" => &count/1
     }
   end
 
-  def add([first, second]), do: first + second
-  def subtract([first, second]), do: first - second
-  def multiply([first, second]), do: first * second
-  def divide([first, second]), do: div(first, second)
+  def list?([arg]) when is_list(arg), do: true
+  def list?([_arg]), do: false
+
+  def empty?([[]]), do: true
+  def empty?(_), do: false
+
+  def count([arg]) when is_list(arg), do: length(arg)
+  def count(_), do: 0
 end
