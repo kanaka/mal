0fab722c42e16daea47705091b384dadc66cc124
ekmartin <mail@ekmartin.no>
2015-09-06 18:44:14 +0200

    Rely more on Kernel.inspect for printing

--- elixir/lib/mal/printer.ex
+++ elixir/lib/mal/printer.ex
@@ -2,13 +2,11 @@ defmodule Mal.Printer do
   def print_str(mal, print_readably \\ true)
   def print_str(mal, _) when is_atom(mal), do: Atom.to_string(mal)
   def print_str(mal, _) when is_integer(mal), do: Integer.to_string(mal)
-  def print_str(mal, _) when is_function(mal), do: "#<function>"
-  def print_str({:closure, _function}, _), do: "#<function>"
+  def print_str(mal, _) when is_function(mal), do: inspect(mal)
+  def print_str({:closure, mal}, _), do: inspect(mal)
   def print_str({:symbol, value}, _), do: value
   def print_str(mal, false) when is_bitstring(mal), do: mal
-  def print_str(mal, true) when is_bitstring(mal) do
-    Inspect.Algebra.to_doc(mal, %Inspect.Opts{})
-  end
+  def print_str(mal, true) when is_bitstring(mal), do: inspect(mal)
 
   def print_str(mal, print_readably) when is_list(mal) do
     output = mal
