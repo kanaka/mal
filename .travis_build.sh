0ff53c8d0a6a5aacc5d63f925b8a65f8b40a4817
Joel Martin <github@martintribe.org>
2015-10-10 10:34:46 -0500

    Travis: build rpython twice to workaround errors.
    To get around this error which often happens in step9 build after
    first 8 steps have been built:
    
    .......RPython traceback:
      File "rpython_jit_metainterp_compile.c", line 21356, in send_loop_to_backend
      File "rpython_jit_backend_x86_assembler.c", line 1905, in Assembler386_assemble_loop
      File "rpython_jit_backend_x86_regalloc.c", line 309, in RegAlloc_prepare_loop
      File "rpython_jit_backend_x86_regalloc.c", line 978, in RegAlloc__prepare
      File "rpython_jit_backend_llsupport_regalloc.c", line 6519, in compute_vars_longevity
    Fatal RPython error: AssertionError
    Makefile:14: recipe for target 'step9_try' failed

--- .travis_build.sh
+++ .travis_build.sh
@@ -2,10 +2,11 @@
 
 set -ex
 
+BUILD_IMPL=${BUILD_IMPL:-${IMPL}}
+
 # If NO_DOCKER is blank then launch use a docker image, otherwise
 # use the Travis image/tools directly.
 if [ -z "${NO_DOCKER}" ]; then
-    BUILD_IMPL=${BUILD_IMPL:-${IMPL}}
     impl=$(echo "${IMPL}" | tr '[:upper:]' '[:lower:]')
     build_impl=$(echo "${BUILD_IMPL}" | tr '[:upper:]' '[:lower:]')
 
@@ -13,7 +14,12 @@ if [ -z "${NO_DOCKER}" ]; then
     if [ "${impl}" != "${build_impl}" ]; then
         docker pull kanaka/mal-test-${build_impl}
     fi
+    if [ "${BUILD_IMPL}" = "rpython" ]; then
+        # rpython often fails once per build in compute_vars_longevity
+        # so build it twice and ignore failures the first time
+        docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${build_impl} make -C ${BUILD_IMPL} || true
+    fi
     docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${build_impl} make -C ${BUILD_IMPL}
 else
-    make -C ${IMPL}
+    make -C ${BUILD_IMPL}
 fi
