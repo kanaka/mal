531a98a931621c19ffc9a95f7b0b1124defaf225
Joel Martin <github@martintribe.org>
2015-10-10 10:54:08 -0500

    Travis: add self-hosted testing.

--- .travis_test.sh
+++ .travis_test.sh
@@ -2,14 +2,23 @@
 
 set -ex
 
+ACTION=${1}
+IMPL=${2}
+MAL_IMPL=${3:-js}
+
+echo "ACTION: ${ACTION}"
+echo "IMPL: ${IMPL}"
+echo "MAL_IMPL: ${MAL_IMPL}"
+
 # If NO_DOCKER is blank then launch use a docker image, otherwise
 # use the Travis image/tools directly.
 if [ -z "${NO_DOCKER}" ]; then
     impl=$(echo "${IMPL}" | tr '[:upper:]' '[:lower:]')
 
-    docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${impl} make TEST_OPTS="--soft --log-file ../test.out" test^${IMPL}
-    #docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${IMPL,,} make perf^${IMPL}
+    docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${impl} \
+        make TEST_OPTS="--soft --log-file ../${ACTION}.out" \
+            MAL_IMPL=${MAL_IMPL} ${ACTION}^${IMPL}
 else
-    make TEST_OPTS="--soft --log-file ../test.out" test^${IMPL}
-    #make TEST_OPTS="--soft --log-file ../perf" perf^${IMPL}
+    make TEST_OPTS="--soft --log-file ../${ACTION}.out" \
+        MAL_IMPL=${MAL_IMPL} ${ACTION}^${IMPL}
 fi
