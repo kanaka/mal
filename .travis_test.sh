6ce7b0ed25cc7bf598b2825ee80e8fe7a359114e
Joel Martin <github@martintribe.org>
2015-10-25 16:17:09 -0500

    Travis: add perf test.

--- .travis_test.sh
+++ .travis_test.sh
@@ -10,18 +10,19 @@ echo "ACTION: ${ACTION}"
 echo "IMPL: ${IMPL}"
 echo "MAL_IMPL: ${MAL_IMPL}"
 
-# If NO_DOCKER is blank then launch use a docker image, otherwise
-# use the Travis image/tools directly.
-if [ -z "${NO_DOCKER}" ]; then
+# If NO_DOCKER is blank then launch use a docker image, otherwise use
+# the Travis image/tools directly.
+if [ "${NO_DOCKER}" ]; then
+    MAKE="make"
+else
     impl=$(echo "${IMPL}" | tr '[:upper:]' '[:lower:]')
     img_impl=$(echo "${3:-${IMPL}}" | tr '[:upper:]' '[:lower:]')
 
-    docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${img_impl} \
-        make TEST_OPTS="--soft --log-file ../${ACTION}.err" \
-            MAL_IMPL=${MAL_IMPL} ${ACTION}^${IMPL}
-else
-    make TEST_OPTS="--soft --log-file ../${ACTION}.err" \
-        MAL_IMPL=${MAL_IMPL} ${ACTION}^${IMPL}
+    MAKE="docker run -it -u $(id -u) -v `pwd`:/mal kanaka/mal-test-${img_impl} make"
 fi
+
+${MAKE} TEST_OPTS="--soft --log-file ../${ACTION}.err" \
+    MAL_IMPL=${MAL_IMPL} ${ACTION}^${IMPL}
+
 # no failure so remove error log
 rm ${ACTION}.err
