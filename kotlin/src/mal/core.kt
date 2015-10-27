6132da853dc299de9434af6e45dcde57fa26e6c2
Javier Fernandez-Ivern <javier.ivern@gmail.com>
2015-10-26 21:49:58 -0500

    Fixed final bug with swap, added tests

--- kotlin/src/mal/core.kt
+++ kotlin/src/mal/core.kt
@@ -180,19 +180,12 @@ val ns = hashMapOf(
             value
         })),
         Pair(MalSymbol("swap!"), MalFunction({ a: ISeq ->
-            val atom = (a.nth(0) as MalAtom)
+            val atom = a.nth(0) as MalAtom
             val function = a.nth(1) as MalFunction
 
-            // TODO reuse code from apply?
             val params = MalList()
             params.conj_BANG(atom.value)
-            a.seq().drop(2).forEach({ it ->
-                if (it is ISeq && it.seq().any()) {
-                    it.seq().forEach({ x -> params.conj_BANG(x) })
-                } else {
-                    params.conj_BANG(it)
-                }
-            })
+            a.seq().drop(2).forEach({ it -> params.conj_BANG(it) })
 
             val value = function.apply(params)
             atom.value = value
