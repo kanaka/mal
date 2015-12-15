3c1b95d2b31da01717efa8141908c680603c1222
Dov Murik <dov.murik@gmail.com>
2015-12-15 12:52:47 -0500

    guile: use readable/not-readable when printing hash-maps

--- guile/printer.scm
+++ guile/printer.scm
@@ -25,7 +25,7 @@
       (string-join
        (hash-map->list
         (lambda (k v)
-          (format #f "~a ~a" (pr_str k #t) (pr_str v #t)))
+          (format #f "~a ~a" (p k) (p v)))
         hm)
        " ")
       port)
