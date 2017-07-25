74771ea2a9a58e07f71c3213957fb60d27dc91c8
Joel Martin <github@martintribe.org>
2017-07-25 11:13:13 -0500

    plpgsql: support Travis run with alt. user
    Seems that Travis sometimes runs with a user other than 1001 so add
    user ID 2000 as well.

--- plpgsql/entrypoint.sh
+++ plpgsql/entrypoint.sh
@@ -9,9 +9,9 @@ while [[ ${1:0:1} = '-' ]]; do
 done
 
 sudo --user=${POSTGRES_SUDO_USER} \
-    /usr/lib/postgresql/9.4/bin/postgres \
+    bash -c "/usr/lib/postgresql/9.4/bin/postgres \
     -c config_file=/etc/postgresql/9.4/main/postgresql.conf \
-    ${POPTS} >/var/log/postgresql/output.log 2>&1 & disown -h
+    ${POPTS} >/var/log/postgresql/output.log 2>&1" & disown -h
 
 while ! ( echo "" > /dev/tcp/localhost/5432) 2>/dev/null; do
     echo "Waiting for postgres to start"
