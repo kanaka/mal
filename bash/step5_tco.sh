5bbc7a1fb89d03467ab53b3810ca2ba501798da4
Joel Martin <github@martintribe.org>
2017-10-11 15:57:07 -0400

    bash, basic: add number?, fn?, and macro?

--- bash/step5_tco.sh
+++ bash/step5_tco.sh
@@ -50,9 +50,10 @@ EVAL () {
         EVAL_AST "${ast}" "${env}"
         return
     fi
-    _empty? "${ast}" && r="${ast}" && return
 
     # apply list
+    _empty? "${ast}" && r="${ast}" && return
+
     _nth "${ast}" 0; local a0="${r}"
     _nth "${ast}" 1; local a1="${r}"
     _nth "${ast}" 2; local a2="${r}"
@@ -61,7 +62,7 @@ EVAL () {
               [[ "${__ERROR}" ]] && return 1
               ENV_SET "${env}" "${a1}" "${r}"
               return ;;
-        let*) ENV "${env}"; local let_env="${r}"
+        let__STAR__) ENV "${env}"; local let_env="${r}"
               local let_pairs=(${ANON["${a1}"]})
               local idx=0
               #echo "let: [${let_pairs[*]}] for ${a2}"
@@ -99,7 +100,7 @@ EVAL () {
               fi
               # Continue loop
               ;;
-        fn*)  _function "ENV \"${env}\" \"${a1}\" \"\${@}\"; \
+        fn__STAR__)  _function "ENV \"${env}\" \"${a1}\" \"\${@}\"; \
                          EVAL \"${a2}\" \"\${r}\"" \
                         "${a2}" "${env}" "${a1}"
               return ;;
