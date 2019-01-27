08f92c5ae9f477019849195e6937b591d3d3f6d5
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-01-27 18:21:57 +0000

    upgrade clojure and tools.reader

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,8 +1,8 @@
 (defproject mal "0.0.1-SNAPSHOT"
   :description "Make-A-Lisp"
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.reader "0.8.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.reader "1.3.2"]
                  [net.n01se/clojure-jna "1.0.0"]]
 
   ;; To run a step with correct readline behavior:
