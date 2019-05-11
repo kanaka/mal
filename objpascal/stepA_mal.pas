94a6f915679805cb327df2a5af2a9387f8955ac9
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2019-05-11 17:43:17 +0200

    gensym: changes forgotten in last commit

--- objpascal/stepA_mal.pas
+++ objpascal/stepA_mal.pas
@@ -316,7 +316,7 @@ begin
     REP('(def! load-file (fn* (f) (eval (read-string (str "(do " (slurp f) ")")))))');
     REP('(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list ''if (first xs) (if (> (count xs) 1) (nth xs 1) (throw "odd number of forms to cond")) (cons ''cond (rest (rest xs)))))))');
     REP('(def! inc (fn* [x] (+ x 1)))');
-    REP('(def! gensym (let* [counter (atom 0)] (fn* [] (symbol (str \"G__\" (swap! counter inc))))))');
+    REP('(def! gensym (let* [counter (atom 0)] (fn* [] (symbol (str "G__" (swap! counter inc))))))');
     REP('(defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) (let* (condvar (gensym)) `(let* (~condvar ~(first xs)) (if ~condvar ~condvar (or ~@(rest xs)))))))))');
 
 
