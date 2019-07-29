d0f8032386a04f4a05bf553e544db786d205c03b
Ben Harris <bjh21@bjh21.me.uk>
2019-07-30 00:01:15 +0100

    perl: Remove experimental given/when syntax from interop.pm.
    It doesn't gain anything over for/if, and removing it avoids a warning.

--- perl/interop.pm
+++ perl/interop.pm
@@ -1,8 +1,6 @@
 package interop;
 use strict;
 use warnings;
-no if $] >= 5.018, warnings => "experimental::smartmatch";
-use feature qw(switch);
 
 use Exporter 'import';
 our @EXPORT_OK = qw( pl_to_mal );
@@ -12,19 +10,17 @@ use types qw($nil);
 
 sub pl_to_mal {
     my($obj) = @_;
-    given (ref $obj) {
-        when(/^ARRAY/) {
+    for (ref $obj) {
+        if (/^ARRAY/) {
             my @arr = map {pl_to_mal($_)} @$obj;
             return Mal::List->new(\@arr);
-        }
-        when(/^HASH/) {
+        } elsif (/^HASH/) {
             my $hsh = {};
             foreach my $key (keys %$obj) {
                 $hsh->{$key} = pl_to_mal($obj->{$key});
             }
             return Mal::HashMap->new($hsh)
-        }
-        default {
+        } else {
 	    if (!defined($obj)) {
 		return $nil;
             } elsif (looks_like_number($obj)) {
