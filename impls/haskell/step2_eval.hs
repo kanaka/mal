c9c504ac205eb209f375a1b3c9c9d32bacd30813
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2020-08-24 10:38:48 +0200

    haskell: make grammar readable as reference, misc
    Split user functions and macros, merge user functions and core functions.
    
    Add a flag triggering debugging info in EVAL.
    
    Reserve mutable environments for REPL and let*.
    Move env type declaration from Types to Env.
    Check let* arguments only once.
    
    Share more code between map constructions and key type checks.
    
    Stop copying metadata when evaluating collections.
    
    The strict variant of Data.Map.Strict is recommended for general use.
    
    simplify printer.

--- impls/haskell/step2_eval.hs
+++ impls/haskell/step2_eval.hs
@@ -1,13 +1,17 @@
 import System.IO (hFlush, stdout)
-import Control.Monad ((<=<))
-import Control.Monad.Except (runExceptT)
-import Control.Monad.Trans (liftIO)
+import Control.Monad.Except (liftIO, runExceptT)
 import qualified Data.Map as Map
 
 import Readline (addHistory, readline, load_history)
 import Types
 import Reader (read_str)
-import Printer (_pr_str)
+import Printer (_pr_list, _pr_str)
+
+--
+--  Set this to True for a trace of each call to Eval.
+--
+traceEval :: Bool
+traceEval = False
 
 -- read
 
@@ -16,32 +20,35 @@ mal_read = read_str
 
 -- eval
 
--- eval_ast is replaced with pattern matching.
-
-apply_ast :: [MalVal] -> IOThrows MalVal
-
-apply_ast [] = return $ toList []
-
-apply_ast ast = do
-    evd <- mapM eval ast
+apply_ast :: MalVal -> [MalVal] -> IOThrows MalVal
+apply_ast first rest = do
+    evd <- eval first
     case evd of
-        MalFunction {fn=f} : args -> f args
-        _ -> throwStr . (++) "invalid apply: " =<< liftIO (Printer._pr_str True (toList ast))
+        MalFunction _ f -> f =<< mapM eval rest
+        _               -> throwStr . (++) "invalid apply: " =<< liftIO (_pr_list True " " $ first : rest)
 
 eval :: MalVal -> IOThrows MalVal
-eval (MalSymbol sym)            = do
-    case Map.lookup sym repl_env of
-        Nothing  -> throwStr $ "'" ++ sym ++ "' not found"
-        Just val -> return val
-eval (MalSeq _ (Vect False) xs) = apply_ast xs
-eval (MalSeq m (Vect True)  xs) = MalSeq m (Vect True) <$> mapM eval xs
-eval (MalHashMap m xs)          = MalHashMap m         <$> mapM eval xs
-eval ast                        = return ast
+eval ast = do
+    case traceEval of
+        True -> liftIO $ do
+            putStr "EVAL: "
+            putStrLn =<< _pr_str True ast
+            hFlush stdout
+        False -> pure ()
+    case ast of
+        MalSymbol sym -> do
+            case Map.lookup sym repl_env of
+                Nothing  -> throwStr $ "'" ++ sym ++ "' not found"
+                Just val -> return val
+        MalSeq _ (Vect False) (a1 : as) -> apply_ast a1 as
+        MalSeq _ (Vect True)  xs        -> MalSeq (MetaData Nil) (Vect True) <$> mapM eval xs
+        MalHashMap _ xs                 -> MalHashMap (MetaData Nil) <$> mapM eval xs
+        _ -> return ast
 
 -- print
 
 mal_print :: MalVal -> IOThrows String
-mal_print = liftIO. Printer._pr_str True
+mal_print = liftIO . Printer._pr_str True
 
 -- repl
 
@@ -68,7 +75,7 @@ repl_env = Map.fromList [("+", _func add),
                          ("/", _func divd)]
 
 rep :: String -> IOThrows String
-rep = mal_print <=< eval <=< mal_read
+rep line = mal_print =<< eval =<< mal_read line
 
 repl_loop :: IO ()
 repl_loop = do
@@ -87,7 +94,7 @@ repl_loop = do
             repl_loop
 
 _func :: Fn -> MalVal
-_func f = MalFunction {fn=f, f_ast=Nil, f_params=[], macro=False, meta=Nil}
+_func f = MalFunction (MetaData Nil) f
 
 main :: IO ()
 main = do
