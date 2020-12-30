46d997e994f06978ae47fde3ce14183700d28554
Graeme Lockley <graemel@no9.co.za>
2020-12-30 15:44:03 +0200

    feature: step 3 environments

--- impls/deno/env.ts
+++ impls/deno/env.ts
@@ -1,6 +1,37 @@
 import * as MalType from "./types.ts";
 
-export type Env = Map<string, MalType.MalType>;
+export type Scope = Map<string, MalType.MalType>;
 
-export const lookup = (name: string, env: Env): MalType.MalType | undefined =>
-  env.get(name);
+export type Env = {
+  outer: Env | undefined;
+  data: Scope;
+};
+
+export const mkEnv = (outer: Env | undefined = undefined): Env => ({
+  outer,
+  data: new Map(),
+});
+
+export const find = (name: string, env: Env): MalType.MalType | undefined => {
+  const result = env.data.get(name);
+
+  if (result === undefined) {
+    return env.outer === undefined ? undefined : find(name, env.outer);
+  } else {
+    return result;
+  }
+};
+
+export const get = (name: string, env: Env): MalType.MalType => {
+  const result = find(name, env);
+
+  if (result === undefined) {
+    throw new Error(`Undefined Symbol: ${name} not found`);
+  } else {
+    return result;
+  }
+};
+
+export const set = (name: string, value: MalType.MalType, env: Env): void => {
+  env.data.set(name, value);
+};
--- impls/deno/step3_env.ts
+++ impls/deno/step3_env.ts
@@ -41,13 +41,7 @@ const read = (str: string): MalType.MalType => Reader.readStr(str);
 
 const evaluate_ast = (ast: MalType.MalType, env: Env.Env): MalType.MalType => {
   if (ast.tag === "MalSymbol") {
-    const binding = Env.find(ast.name, env);
-
-    if (binding === undefined) {
-      throw new Error(`Unknown Symbol: ${ast.name}`);
-    } else {
-      return binding;
-    }
+    return Env.get(ast.name, env);
   } else if (ast.tag === "MalList") {
     return MalType.mkList(ast.items.map((i) => evaluate(i, env)));
   } else if (ast.tag === "MalVector") {
@@ -61,10 +55,51 @@ const evaluate_ast = (ast: MalType.MalType, env: Env.Env): MalType.MalType => {
   }
 };
 
+const isNamedSymbol = (ast: MalType.MalType, name: string): boolean =>
+  ast.tag === "MalSymbol" && ast.name === name;
+
 const evaluate = (ast: MalType.MalType, env: Env.Env): MalType.MalType => {
   if (ast.tag === "MalList") {
     if (ast.items.length === 0) {
       return ast;
+    } else if (isNamedSymbol(ast.items[0], "def!")) {
+      if (ast.items[1].tag === "MalSymbol") {
+        const result = evaluate(ast.items[2], env);
+        Env.set(ast.items[1].name, result, env);
+        return result;
+      } else {
+        throw new Error(
+          `Invalid Argument: def! requires a symbol name: ${ast.items[1]}`,
+        );
+      }
+    } else if (isNamedSymbol(ast.items[0], "let*")) {
+      const innerEnv = Env.mkEnv(env);
+
+      const bindings = ast.items[1];
+      if (bindings.tag === "MalList" || bindings.tag === "MalVector") {
+        for (let lp = 0; lp < bindings.items.length; lp += 2) {
+          const name = bindings.items[lp];
+          const value = bindings.items[lp + 1] ?? MalType.nil;
+
+          if (name.tag === "MalSymbol") {
+            Env.set(name.name, evaluate(value, innerEnv), innerEnv);
+          } else {
+            throw new Error(
+              `Invalid Argument: let* binding requires a symbol name: ${
+                JSON.stringify(name)
+              }`,
+            );
+          }
+        }
+
+        return evaluate(ast.items[2], innerEnv);
+      } else {
+        throw new Error(
+          `Invalid Argument: let* requires a list of bindings: ${
+            JSON.stringify(bindings)
+          }`,
+        );
+      }
     } else {
       const evalList = evaluate_ast(ast, env);
 
@@ -75,9 +110,9 @@ const evaluate = (ast: MalType.MalType, env: Env.Env): MalType.MalType => {
           if (callerItem.tag === "MalFunction") {
             return callerItem.f(callerArgs);
           } else if (callerItem.tag === "MalSymbol") {
-            const binding = Env.find(callerItem.name, env);
+            const binding = Env.get(callerItem.name, env);
 
-            if (binding !== undefined && binding.tag === "MalFunction") {
+            if (binding.tag === "MalFunction") {
               return binding.f(callerArgs);
             }
           }
@@ -93,8 +128,6 @@ const evaluate = (ast: MalType.MalType, env: Env.Env): MalType.MalType => {
 const print = (exp: MalType.MalType): string => Printer.prStr(exp);
 
 const rep = (str: string): string => print(evaluate(read(str), replEnv));
-
-type FunctionType = (a: MalType.MalType, b: MalType.MalType) => MalType.MalType;
 
 while (true) {
   const value = prompt("user>");
