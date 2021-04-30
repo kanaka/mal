b4581bbb9b357bbd1af6871e61aeb0a888a78615
jpaquim <joaopaquim@gmail.com>
2021-04-30 19:16:29 +0100

    Add zig init-exe template and build files

---
+++ impls/zig-mal/build.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard release options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const mode = b.standardReleaseOptions();
+
+    const exe = b.addExecutable("step0_repl", "src/step0_repl.zig");
+    exe.setTarget(target);
+    exe.setBuildMode(mode);
+    exe.setOutputDir(".");
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step(exe.name, exe.name);
+    run_step.dependOn(&run_cmd.step);
+}
