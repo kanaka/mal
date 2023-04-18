87b0b23970ff8b6d61a3363b4a62730ce4753f3f
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2023-04-18 19:42:58 +0200

    perl: backport all recent style changes from stepA to previous steps

--- impls/perl/step1_read_print.pl
+++ impls/perl/step1_read_print.pl
@@ -1,58 +1,52 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
-use File::Basename;
-use lib dirname (__FILE__);
+use File::Basename 'dirname';
+use lib dirname(__FILE__);
 
-use Scalar::Util qw(blessed);
+use English '-no_match_vars';
 
-use readline qw(mal_readline set_rl_mode);
-use reader;
-use printer;
+use Readline qw(mal_readline set_rl_mode);
+use Reader   qw(read_str);
+use Printer  qw(pr_str);
 
 # read
 sub READ {
     my $str = shift;
-    return reader::read_str($str);
+    return read_str($str);
 }
 
 # eval
 sub EVAL {
-    my($ast, $env) = @_;
+    my ($ast) = @_;
     return $ast;
 }
 
 # print
 sub PRINT {
     my $exp = shift;
-    return printer::_pr_str($exp);
+    return pr_str($exp);
 }
 
 # repl
 sub REP {
     my $str = shift;
-    return PRINT(EVAL(READ($str), {}));
+    return PRINT( EVAL( READ($str) ) );
 }
 
-if (@ARGV && $ARGV[0] eq "--raw") {
-    set_rl_mode("raw");
+# Command line arguments
+if ( $ARGV[0] eq '--raw' ) {
+    set_rl_mode('raw');
+    shift @ARGV;
 }
-while (1) {
-    my $line = mal_readline("user> ");
-    if (! defined $line) { last; }
-    do {
-        local $@;
-        my $ret;
-        eval {
-            print(REP($line), "\n");
-            1;
-        } or do {
-            my $err = $@;
-            if (defined(blessed $err) && $err->isa('Mal::BlankException')) {
-		# ignore and continue
-	    } else {
-		chomp $err;
-		print "Error: $err\n";
-            }
-        };
+
+while ( defined( my $line = mal_readline('user> ') ) ) {
+    eval {
+        print REP($line), "\n" or die $ERRNO;
+        1;
+    } or do {
+        my $err = $EVAL_ERROR;
+        print 'Error: ', $err or die $ERRNO;
     };
 }
