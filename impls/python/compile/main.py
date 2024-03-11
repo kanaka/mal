4b537f7ed617ad1d8c921d3fea73ebc505ab2a65
jcguu95 <jcguu95@gmail.com>
2024-03-11 10:08:48 -0400

    Use a real debug logger.

--- impls/python/compile/main.py
+++ impls/python/compile/main.py
@@ -5,6 +5,8 @@ import reader, printer
 from env import Env
 
 # debug
+from loguru import logger
+logger.info("LOGURU as debugger.")
 _line_history = []
 _ast_history = []
 _wake_up_command = ";()"
@@ -15,31 +17,20 @@ sys.ps2 = "  > "
 def READ(str):
     return reader.read_str(str)
 
-# # TODO Cheat a bit here. Remove later..
-# def eval_ast(ast, env):
-#     if types._symbol_Q(ast):
-#         return env.get(ast)
-#     elif types._list_Q(ast):
-#         return types._list(*map(lambda a: EVAL(a, env), ast))
-#     elif types._vector_Q(ast):
-#         return types._vector(*map(lambda a: EVAL(a, env), ast))
-#     elif types._hash_map_Q(ast):
-#         return types.Hash_Map((k, EVAL(v, env)) for k, v in ast.items())
-#     else:
-#         return ast  # primitive value, return unchanged
-
 def EXEC_COMPILE (ast, env):
     def COMPILE (ast, env):
-        print("[DEBUG] (ast):", ast)
+        logger.debug(f"ast: {ast}")
         if types._symbol_Q(ast):
             compiled_string="_RETURNED_OBJECT = exec_env.get(exec_ast)"
-        elif not types._list_Q(ast):
-            compiled_string="_RETURNED_OBJECT = exec_ast"
-        elif ast[0] == "def!":
-            compiled_string="_RETURNED_OBJECT = exec_env.set(exec_ast[1], EXEC_COMPILE(exec_ast[2], exec_env))"
+        elif types._list_Q(ast):
+            if ast[0] == "def!":
+                compiled_string="_RETURNED_OBJECT = exec_env.set(exec_ast[1], EXEC_COMPILE(exec_ast[2], exec_env))"
+            else:
+                compiled_string="_RETURNED_OBJECT = exec_ast"
         else:
+            logger.debug(f"TODO Unsupported Area")
             compiled_string="_RETURNED_OBJECT = exec_ast"
-        print("[DEBUG] (compiled_string):", compiled_string)
+        logger.debug(f"compiled_string: {compiled_string}")
         return compiled_string
 
     def EXEC (compiled_string):
@@ -55,7 +46,8 @@ def EVAL(ast, env):
     global _ast_history
     _ast_history.append(ast)
     #print("EVAL %s" % printer._pr_str(ast))
-    return ast
+    # return ast
+    return EXEC_COMPILE(ast, env)
 
 # print
 def PRINT(exp):
@@ -69,14 +61,14 @@ def REP(str):
 # lisp repl loop
 def REPL():
     global _line_history
-    print(f"Hint: Use `{_wake_up_command}` to get into PYTHON.")
+    logger.info(f"Hint: Use `{_wake_up_command}` to get into PYTHON.")
     while True:
         try:
             line = mal_readline.readline("LISP> ")
             if line == None: break
             if line == "": continue
             if line == _wake_up_command:
-                print(f"Hint: Use `LISP()` to get into LISP.")
+                logger.info(f"Hint: Use `LISP()` to get into LISP.")
                 break # debug
             _line_history.append(line) # debug
             print(REP(line))
