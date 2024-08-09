31fa6bc9177d3705ca03cefb013788b7a0fb4746
老刘 <632171029@qq.com>
2024-08-09 19:12:09 +0800

    vbs: Merge branch 'kanaka:master' into master

--- impls/python/step4_if_fn_do.py
+++ impls/python/step4_if_fn_do.py
@@ -6,19 +6,18 @@ from env import Env
 import core
 
 # read
-def READ(str):
-    return reader.read_str(str)
+READ = reader.read_str
 
 # eval
 def EVAL(ast, env):
-    dbgeval = env.get_or_nil('DEBUG-EVAL')
+    dbgeval = env.get(types._symbol('DEBUG-EVAL'), return_nil=True)
     if dbgeval is not None and dbgeval is not False:
         print('EVAL: ' + printer._pr_str(ast))
 
     if types._symbol_Q(ast):
         return env.get(ast)
     elif types._vector_Q(ast):
-        return types._vector(*map(lambda a: EVAL(a, env), ast))
+        return types.Vector(EVAL(a, env) for a in ast)
     elif types._hash_map_Q(ast):
         return types.Hash_Map((k, EVAL(v, env)) for k, v in ast.items())
     elif not types._list_Q(ast):
@@ -29,6 +28,7 @@ def EVAL(ast, env):
         if len(ast) == 0: return ast
         a0 = ast[0]
 
+    if types._symbol_Q(a0):
         if "def!" == a0:
             a1, a2 = ast[1], ast[2]
             res = EVAL(a2, env)
@@ -36,8 +36,8 @@ def EVAL(ast, env):
         elif "let*" == a0:
             a1, a2 = ast[1], ast[2]
             let_env = Env(env)
-            for i in range(0, len(a1), 2):
-                let_env.set(a1[i], EVAL(a1[i+1], let_env))
+            for k, v in types.asPairs(a1):
+                let_env.set(k, EVAL(v, let_env))
             return EVAL(a2, let_env)
         elif "do" == a0:
             for i in range(1, len(ast)-1):
@@ -47,21 +47,27 @@ def EVAL(ast, env):
             a1, a2 = ast[1], ast[2]
             cond = EVAL(a1, env)
             if cond is None or cond is False:
-                if len(ast) > 3: return EVAL(ast[3], env)
-                else:            return None
+                if len(ast) > 3:
+                    return EVAL(ast[3], env)
+                else:
+                    return None
             else:
                 return EVAL(a2, env)
         elif "fn*" == a0:
             a1, a2 = ast[1], ast[2]
-            return types._function(EVAL, Env, a2, env, a1)
-        else:
-            f = EVAL(a0, env)
+            def fn(*args):
+                return EVAL(a2, Env(env, a1, args))
+            return fn
+
+    f = EVAL(a0, env)
+    if types._function_Q(f):
             args = ast[1:]
             return f(*(EVAL(a, env) for a in args))
+    else:
+        raise Exception('Can only apply functions')
 
 # print
-def PRINT(exp):
-    return printer._pr_str(exp)
+PRINT = printer._pr_str
 
 # repl
 repl_env = Env()
@@ -78,9 +84,10 @@ REP("(def! not (fn* (a) (if a false true)))")
 while True:
     try:
         line = mal_readline.readline("user> ")
-        if line == None: break
-        if line == "": continue
         print(REP(line))
+    except EOFError:
+        print()
+        break
     except reader.Blank: continue
-    except Exception as e:
+    except Exception:
         print("".join(traceback.format_exception(*sys.exc_info())))
