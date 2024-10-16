a9a6d9068acc06155a3e2286bff10f70bcb6c65a
Nicolas Boulenguez <nicolas.boulenguez@free.fr>
2024-10-16 09:11:13 +0200

    rpython: simplify and probably remove a performance bottleneck
    Traverse environments without recursion.  Unwrap the key once.
    Do not wrap DEBUG-EVAL on each evaluation.
    Spare two intermediate MalLists for the 'do' special form.

--- impls/rpython/step4_if_fn_do.py
+++ impls/rpython/step4_if_fn_do.py
@@ -14,19 +14,15 @@ def READ(str):
     return reader.read_str(str)
 
 # eval
-def eval_ast(ast, env):
-    assert isinstance(ast, MalList)
-    res = []
-    for a in ast.values:
-        res.append(EVAL(a, env))
-    return MalList(res)
-
 def EVAL(ast, env):
-    if env.get_or_None(MalSym(u"DEBUG-EVAL")) not in (None, nil, false):
+    if env.get(u"DEBUG-EVAL") not in (None, nil, false):
         print(u"EVAL: " + printer._pr_str(ast))
     if types._symbol_Q(ast):
         assert isinstance(ast, MalSym)
-        return env.get(ast)
+        value = env.get(ast.value)
+        if value is None:
+            throw_str("'" + str(ast.value) + "' not found")
+        return value
     elif types._vector_Q(ast):
         res = []
         for a in ast.values:
@@ -59,8 +55,11 @@ def EVAL(ast, env):
                 let_env.set(a1[i], EVAL(a1[i+1], let_env))
             return EVAL(a2, let_env)
         elif u"do" == a0sym:
-            el = eval_ast(ast.rest(), env)
-            return el.values[-1]
+            if len(ast) == 0:
+                return nil
+            for i in range(1, len(ast) - 1):
+                EVAL(ast[i], env)
+            return EVAL(ast[-1], env)
         elif u"if" == a0sym:
             a1, a2 = ast[1], ast[2]
             cond = EVAL(a1, env)
@@ -73,10 +72,13 @@ def EVAL(ast, env):
             a1, a2 = ast[1], ast[2]
             return MalFunc(None, a2, env, a1, EVAL)
         else:
-            el = eval_ast(ast, env)
-            f = el.values[0]
+            f = EVAL(a0, env)
+            args_list = []
+            for i in range(1, len(ast)):
+                args_list.append(EVAL(ast[i], env))
+            args = MalList(args_list)
             if isinstance(f, MalFunc):
-                return f.apply(el.rest())
+                return f.apply(args)
             else:
                 raise Exception("%s is not callable" % f)
 
